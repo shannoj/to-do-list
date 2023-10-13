1a6139816336e1a9f676d573660ec5c9d5f7a4d4
shannoj <69207315+shannoj@users.noreply.github.com>
2023-10-13 10:45:29 +0200

    created add button, side bar, and main task area

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import _ from 'lodash';
 import './style.css';
+
+function createElements(){
+    const container = document.createElement('div');
+    container.setAttribute('id', 'container');
+
+    const sideBar = document.createElement('div');
+    sideBar.setAttribute('id', 'side-bar');
+
+    container.appendChild(sideBar);
+
+    const taskArea = document.createElement('div');
+    taskArea.setAttribute('id', 'task-area');
+
+    container.appendChild(taskArea);
+
+    const sideBarTasks = document.createElement('div');
+    sideBarTasks.setAttribute('id', 'side-bar-tasks');
+    sideBar.appendChild(sideBarTasks);
+
+    const addButton = document.createElement('button');
+    addButton.setAttribute('id', 'add-button');
+    addButton.innerHTML = "+";
+
+    sideBar.appendChild(addButton);
+
+    return container;
+}
+
+document.body.appendChild(createElements());
