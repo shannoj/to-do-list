042afd6be06b5faa2d6b5854ce34371fbec4125e
shannoj <69207315+shannoj@users.noreply.github.com>
2023-10-19 10:48:53 +0200

    fixed delete button and added edit function

--- src/index.js
+++ src/index.js
@@ -23,6 +23,225 @@ function removeElement (elementid){
     };
 };
 
+function deleteTask(taskId, taskType, timeStamp){
+        // Remove the task from the respective list
+        if (taskType === 'Project') {
+            projectList = projectList.filter(task => task !== timeStamp);
+        } else if (taskType === 'To Do') {
+            todoList = todoList.filter(task => task !== timeStamp);
+        } else if (taskType === 'Note') {
+            notesList = notesList.filter(task => task !== timeStamp);
+        }
+    
+        // Remove the task from the DOM
+        removeElement(taskId);
+};
+
+// Create a function to handle the editing of a task
+function editTask(task, taskDiv) {
+    // Create an edit form
+    //const editForm = document.createElement('form');
+
+    // Populate the form with existing task details
+    // For example, you can use task.type, task.date, and task.details to set the form input values
+
+    // Add input fields for editing
+    //const editTypeInput = document.createElement('input');
+    //editTypeInput.setAttribute('type', 'text');
+    //editTypeInput.value = task.type;
+
+    //setting up the container for the pop up form to add a new task
+    const popUpContainer = document.createElement('div');
+    popUpContainer.setAttribute('id', 'pop-up-container');
+    
+    //setting up actual task form
+    const taskForm = document.createElement('form');
+    taskForm.setAttribute('id', 'task-form');
+    popUpContainer.appendChild(taskForm);
+
+    //setting up the close button and a container for it
+    const closeButtonContainer = document.createElement('div');
+    closeButtonContainer.setAttribute('id', 'close-button-container');
+    taskForm.appendChild(closeButtonContainer);
+
+    const closeForm = document.createElement('button');
+    closeForm.setAttribute('id', 'close-form');
+    closeForm.setAttribute('type', 'button');
+    closeForm.innerHTML = 'x';
+    closeForm.addEventListener('click', function(){
+        removeElement('pop-up-container');
+    });
+
+    closeButtonContainer.appendChild(closeForm);
+    
+    //setting up a container for the questions of the task form
+    const questionContainer = document.createElement('div');
+    questionContainer.setAttribute('id', 'question-container');
+
+    taskForm.appendChild(questionContainer);
+
+    //creating form questions for the type of task
+    const taskType = document.createElement('div');
+    taskType.setAttribute('id', 'task-type');
+    taskType.value = task.type;
+
+    const taskTypeLabel = document.createElement('label');
+    taskTypeLabel.setAttribute('for', 'type');
+    taskTypeLabel.innerHTML = "Type of Task: ";
+
+    const taskSelectDiv = document.createElement('div');
+    taskSelectDiv.setAttribute('id', 'task-div');
+
+    const taskTypeType = document.createElement('select');
+    taskTypeType.setAttribute('id', 'type');
+    taskTypeType.setAttribute('name', 'type');
+
+    const project = document.createElement('option');
+    project.setAttribute('value', 'project');
+    project.innerHTML = 'Project';
+
+    const toDo = document.createElement('option');
+    toDo.setAttribute('value', 'todo');
+    toDo.innerHTML = 'To Do';
+
+    const note = document.createElement('option');
+    note.setAttribute('value', 'note');
+    note.innerHTML = 'Note';
+
+    taskType.appendChild(taskTypeLabel);
+    taskSelectDiv.appendChild(taskTypeType);
+    taskType.appendChild(taskSelectDiv);
+    taskTypeType.appendChild(project);
+    taskTypeType.appendChild(toDo);
+    taskTypeType.appendChild(note);
+
+    questionContainer.appendChild(taskType);
+
+    //creating form questions for the due date of the task
+    const taskDate = document.createElement('div');
+    taskDate.setAttribute('id', 'task-date');
+
+    const taskDateLabel = document.createElement('label');
+    taskTypeLabel.setAttribute('for', 'date');
+    taskTypeLabel.setAttribute('id', 'task-type-label');
+    taskDateLabel.innerHTML = "Due Date: ";
+
+    const taskDateType = document.createElement('input');
+    taskDateType.setAttribute('type', 'date');
+    taskDateType.setAttribute('id', 'date');
+    taskDateType.value = task.date
+
+    taskDate.appendChild(taskDateLabel);
+    taskDate.appendChild(taskDateType);
+
+    questionContainer.appendChild(taskDate);
+
+    //creating form questions for the details of the task
+    const taskDetails = document.createElement('div');
+    taskType.setAttribute('id', 'task-details');
+
+    const taskDetailsLabel = document.createElement('label');
+    taskDetailsLabel.setAttribute('for', 'details');
+    taskDetailsLabel.innerHTML = 'Details: ';
+
+    const taskDetailsType = document.createElement('input');
+    taskDetailsType.setAttribute('type', 'text');
+    taskDetailsType.setAttribute('id', 'details');
+    taskDetailsType.value = task.details;
+
+    taskDetails.appendChild(taskDetailsLabel);
+    taskDetails.appendChild(taskDetailsType);
+
+    questionContainer.appendChild(taskDetails);
+
+    //createing form questoins for priority level of task
+    const taskPriority = document.createElement('div');
+    taskPriority.setAttribute('id', 'task-Priority');
+    taskPriority.value = task.priority;
+
+    const taskPriorityUrgentLabel = document.createElement('label');
+    const taskPriorityUrgentInput = document.createElement('input');
+
+    taskPriorityUrgentInput.setAttribute('type', 'radio');
+    taskPriorityUrgentInput.setAttribute('value', 'urgent');
+    taskPriorityUrgentInput.setAttribute('name', 'task-priority')
+    taskPriorityUrgentInput.setAttribute('id', 'urgent');
+    taskPriorityUrgentLabel.innerHTML = 'Urgent';
+
+    taskPriorityUrgentLabel.appendChild(taskPriorityUrgentInput);
+
+    const taskPriorityMediumLabel = document.createElement('label');
+    const taskPriorityMediumInput = document.createElement('input');
+
+    taskPriorityMediumInput.setAttribute('type', 'radio');
+    taskPriorityMediumInput.setAttribute('value', 'medium');
+    taskPriorityMediumInput.setAttribute('name', 'task-priority')
+    taskPriorityMediumInput.setAttribute('id', 'medium');
+    taskPriorityMediumLabel.innerHTML = 'Medium';
+
+    taskPriorityMediumLabel.appendChild(taskPriorityMediumInput);
+
+    const taskPriorityRelaxedLabel = document.createElement('label');
+    const taskPriorityRelaxedInput = document.createElement('input');
+
+    taskPriorityRelaxedInput.setAttribute('type', 'radio');
+    taskPriorityRelaxedInput.setAttribute('value', 'relaxed');
+    taskPriorityRelaxedInput.setAttribute('name', 'task-priority')
+    taskPriorityRelaxedInput.setAttribute('id', 'relaxed');
+    taskPriorityRelaxedLabel.innerHTML = 'No Rush';
+
+    taskPriorityRelaxedLabel.appendChild(taskPriorityRelaxedInput);
+
+    taskPriority.appendChild(taskPriorityUrgentLabel);
+    taskPriority.appendChild(taskPriorityMediumLabel);
+    taskPriority.appendChild(taskPriorityRelaxedLabel);
+
+    questionContainer.appendChild(taskPriority);
+
+    const saveButton = document.createElement('button');
+    saveButton.textContent = 'Save';
+
+    // Add an event listener to the save button
+    saveButton.addEventListener('click', function() {
+        // Update the task object with the edited details
+        task.type = editTypeInput.value;
+        task.date = taskDateType.value;
+        task.details = taskDetailsType.value;
+
+        // Remove the edit form
+        taskDiv.removeChild(editForm);
+
+        // Update the display with the edited task
+        displayTasks(tabType);
+    });
+
+    document.body.appendChild(popUpContainer);
+
+    //
+
+    /* const editDateInput = document.createElement('input');
+    editDateInput.setAttribute('type', 'date');
+    editDateInput.value = task.date;
+
+    const editDetailsInput = document.createElement('input');
+    editDetailsInput.setAttribute('type', 'text');
+    editDetailsInput.value = task.details;
+    */
+    // Add a save button to submit the edited task
+
+    /*
+    // Append input fields and save button to the edit form
+    editForm.appendChild(editTypeInput);
+    editForm.appendChild(editDateInput);
+    editForm.appendChild(editDetailsInput);
+    editForm.appendChild(saveButton);
+
+    // Replace the taskDiv contents with the edit form
+    taskDiv.innerHTML = '';
+    taskDiv.appendChild(editForm);
+    */
+}
+
 function displayTasks(tabType){
     const container = document.getElementById('task-area');
     container.innerHTML = '';
@@ -88,13 +307,15 @@ function displayTasks(tabType){
     deleteDiv.setAttribute('id', 'delete-div');
     deleteDiv.appendChild(deletePicture);
     deletePicture.addEventListener('click', function(){
-        removeElement('user-task-'+taskDetails);
+        deleteTask('user-task-'+taskDetails, taskType, task);
     });
 
     const editDiv = document.createElement('div');
     editDiv.setAttribute('id', 'edit-div')
     editDiv.appendChild(editPicture);
-    //editDiv.addEventListener('click', );
+    editPicture.addEventListener('click', function(){
+        editTask(task, taskDiv);
+    });
 
     taskDiv.appendChild(taskName);
     taskDiv.appendChild(taskDetailsDiv);
@@ -196,19 +417,23 @@ function addTask(){
     deleteDiv.setAttribute('id', 'delete-div');
     deleteDiv.appendChild(deletePicture);
     deletePicture.addEventListener('click', function(){
-        removeElement('user-task-'+taskDetails);
+        deleteTask('user-task-'+taskDetails, taskType, timeStamp);
     });
 
     const editDiv = document.createElement('div');
     editDiv.setAttribute('id', 'edit-div')
     editDiv.appendChild(editPicture);
-    //editDiv.addEventListener('click', );
+    editPicture.addEventListener('click', function(){
+        editTask(timeStamp, taskDiv);
+    });
 
     taskDiv.appendChild(taskName);
     taskDiv.appendChild(taskDetailsDiv);
     taskDiv.appendChild(taskDueDateDiv);
     taskDiv.appendChild(editDiv);
     taskDiv.appendChild(deleteDiv);
+
+    displayTasks(taskType.toLowerCase() + 's');
 
     removeElement('pop-up-container');
 };
@@ -235,7 +460,7 @@ function taskPopUp() {
     closeForm.setAttribute('type', 'button');
     closeForm.innerHTML = 'x';
     closeForm.addEventListener('click', function(){
-        removeElement('pop-up-container')
+        removeElement('pop-up-container');
     });
 
     closeButtonContainer.appendChild(closeForm);
